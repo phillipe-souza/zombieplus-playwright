9c4f93b82d5577e76cf7e873f79c9f308286e126
Phillipe Souza <maxz_jf2012@hotmail.com>
2024-11-22 14:10:08 -0300

    Novo caso de teste

--- tests/pages/MoviesPage.js
+++ tests/pages/MoviesPage.js
@@ -1,27 +1,39 @@
 const { expect } = require('@playwright/test');
 
 export class MoviesPage {
-    constructor(page) {
-        this.page = page;
-    }
+	constructor(page) {
+		this.page = page;
+	}
 
-    async isLoggedIn() {
-        await this.page.waitForLoadState('networkidle')
-        await expect(this.page).toHaveURL(/.*admin/)
-    }
+	async isLoggedIn() {
+		await this.page.waitForLoadState('networkidle');
+		await expect(this.page).toHaveURL(/.*admin/);
+	}
 
-    async create(title, overview, company, release_year) {
-        await this.page.locator('a[href$="register"]').click()
+	async goForm() {
+		await this.page.locator('a[href$="register"]').click();
+	}
 
-        await this.page.getByLabel('Titulo do filme').fill(title)
-        await this.page.getByLabel('Sinopse').fill(overview)
+	async submit() {
+		await this.page.getByRole('button', { name: 'Cadastrar' }).click();
+	}
 
-        await this.page.locator('#select_company_id .react-select__indicator').click()
-        await this.page.locator('.react-select__option').filter({hasText:company}).click()
+	async create(title, overview, company, release_year) {
+		await this.goForm();
 
-        await this.page.locator('#select_year .react-select__indicator').click()
-        await this.page.locator('.react-select__option').filter({hasText:release_year}).click()
+		await this.page.getByLabel('Titulo do filme').fill(title);
+		await this.page.getByLabel('Sinopse').fill(overview);
 
-        await this.page.getByRole('button', {name: 'Cadastrar'}).click()
-    }
+		await this.page.locator('#select_company_id .react-select__indicator').click();
+		await this.page.locator('.react-select__option').filter({ hasText: company }).click();
+
+		await this.page.locator('#select_year .react-select__indicator').click();
+		await this.page.locator('.react-select__option').filter({ hasText: release_year }).click();
+
+		await this.page.submit();
+	}
+
+	async alertHaveText(target) {
+		await expect(this.page.locator('.alert')).toHaveText(target);
+	}
 }
